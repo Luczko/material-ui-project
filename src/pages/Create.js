ce83f5d025e6da65a91fd17c2ceecea5e1f57961
Luczko <wlalukasz@gmail.com>
2021-07-19 20:51:13 +0200

    formsAdded

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,27 +4,47 @@ import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import { useState } from "react";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: "violet",
-    "&:hover": {
-      backgroundColor: "blue",
-    },
-  },
-  title: {
-    textDecoration: "underline",
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
   },
 });
 
 export default function Create() {
   const classes = useStyles();
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteDetails, setNoteDetails] = useState("");
+  const [noteTitleError, setNoteTitleError] = useState(false);
+  const [noteDetailsError, setNoteDetailsError] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNoteTitleError(false);
+    setNoteDetailsError(false);
+    if (noteTitle === "") {
+      setNoteTitleError(true);
+    }
+    if (noteDetails === "") {
+      setNoteDetailsError(true);
+    }
+    if (noteTitle && noteDetails) {
+    }
+  };
 
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant='h6'
         color='textSecondary'
         component='h2'
@@ -32,16 +52,56 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setNoteTitle(e.target.value)}
+          className={classes.field}
+          color='secondary'
+          label='Note Title'
+          variant='outlined'
+          fullWidth
+          required
+          error={noteTitleError}
+        />
+        <TextField
+          onChange={(e) => setNoteDetails(e.target.value)}
+          className={classes.field}
+          color='secondary'
+          label='Note Details'
+          variant='outlined'
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={noteDetailsError}
+        />
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel value='money' control={<Radio />} label='Money' />
+            <FormControlLabel value='todos' control={<Radio />} label='Todos' />
+            <FormControlLabel
+              value='reminders'
+              control={<Radio />}
+              label='Reminders'
+            />
+            <FormControlLabel value='work' control={<Radio />} label='Work' />
+          </RadioGroup>
+        </FormControl>
 
-      <Button
-        className={classes.btn}
-        type='submit'
-        color='secondary'
-        variant='contained'
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
+        <Button
+          type='submit'
+          color='secondary'
+          variant='contained'
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+
       <br />
       {/* <AcUnitOutlinedIcon />
       <AcUnitOutlinedIcon color='secondary' fontSize='large' /> */}
